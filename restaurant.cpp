c50c7a6c5f2f55818db39a190519a165b7340d3f
ThanhDuy7 <thanh.daoeth3real07@hcmut.edu.vn>
2023-11-24 00:24:05 +0700

    read file

--- restaurant.cpp
+++ restaurant.cpp
@@ -1,7 +1,42 @@
 #include "main.h"
 
+int MAXSIZE = 0;
+
 void simulate(string filename)
 {
-	cout << "Good Luck";
+	ifstream ss(filename);
+	string str, maxsize, name, num;
+	while(ss >> str)
+	{ 
+		if(str == "MAXSIZE")
+		{
+			ss >> maxsize;
+			MAXSIZE = stoi(maxsize); 
+    	}
+        else if(str == "LAPSE") // RED <NAME> <ENERGY>
+        {
+            ss >> name;
+			cout<<"LAPSE"<<" "<<name<<endl;
+			//LAPSE(name);
+    	} else if (str == "KOKUSEN") {
+			cout<<"KOKUSEN"<<endl;
+			//KOKUSEN();
+		} else if (str == "KEITEIKEN") {
+			ss >> num;
+			cout<<"KEITEIKEN"<<" "<<num<<endl;
+			//KEITEIKEN(stoi(num));
+		} else if (str == "HAND") {
+			cout<<"HAND"<<endl;
+			//HAND();
+		} else if (str == "LIMITLESS") {
+			ss >> num;
+			cout<<"LIMITLESS"<<" "<<num<<endl;
+			//LIMITLESS(stoi(num));
+		} else {	//CLEAVE <NUM>
+			ss >> num;
+			cout<<"CLEAVE"<<" "<<num<<endl;
+			//CLEAVE(stoi(num));
+		}
+	}
 	return;
 }
